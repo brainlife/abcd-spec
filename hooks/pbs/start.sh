9f02fc8ae54befbb13d1b88d98e20af2b02d6e42
Soichi Hayashi <hayashis@iu.edu>
2017-09-22 19:39:05 -0400

    added capabililty to set CPUS and WALLTIME resource requirements for pbs

--- hooks/pbs/start.sh
+++ hooks/pbs/start.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 echo "starting $1 on $HOSTNAME"
-jobid=`qsub -d $PWD $SERVICE_DIR/$1`
+
+[ ! $SERVICE_DIR ] && SERVICE_DIR=$PWD
+
+resource="-l nodes=1"
+[ $CPUS ] && resource=$resource:"ppn=$CPUS"
+[ $WALLTIME ] && resource=$resource:"walltime=$WALLTIMME"
+cmd="qsub $resource -d $PWD $SERVICE_DIR/$1"
+echo $cmd
+jobid=`$cmd`
 echo $jobid > jobid
