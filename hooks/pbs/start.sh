155133e22cc755844878e247eb4c20aca5e47f96
Soichi Hayashi <hayashis@iu.edu>
2017-09-22 20:21:18 -0400

    switched to use getopts

--- hooks/pbs/start.sh
+++ hooks/pbs/start.sh
@@ -5,9 +5,18 @@ echo "starting $1 on $HOSTNAME"
 [ ! $SERVICE_DIR ] && SERVICE_DIR=$PWD
 
 resource="-l nodes=1"
-[ $CPUS ] && resource=$resource:"ppn=$CPUS"
-[ $WALLTIME ] && resource=$resource:"walltime=$WALLTIMME"
-cmd="qsub $resource -d $PWD $SERVICE_DIR/$1"
+while getopts ":s:c:t:" opt; do
+    case "$opt" in
+    s) 	script=$OPTARG
+        ;;
+    c) 	resource=$resource:"ppn=$OPTARG"
+        ;;
+    t)  resource=$resource:"walltime=$OPTARG"
+        ;;
+    esac
+done
+
+cmd="qsub $resource -d $PWD $SERVICE_DIR/$script"
 echo $cmd
 jobid=`$cmd`
 echo $jobid > jobid
