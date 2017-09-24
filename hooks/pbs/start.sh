ad8633965c9bcce717f3ab35fbe68e859c406136
Soichi Hayashi <hayashis@iu.edu>
2017-09-23 21:41:40 -0400

    split walltime from rest of -l param

--- hooks/pbs/start.sh
+++ hooks/pbs/start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "starting $1 on $HOSTNAME"
-
 [ ! $SERVICE_DIR ] && SERVICE_DIR=$PWD
 
 resource="-l nodes=1"
@@ -9,14 +7,17 @@ while getopts ":s:c:t:" opt; do
     case "$opt" in
     s) 	script=$OPTARG
         ;;
-    c) 	resource=$resource:"ppn=$OPTARG"
+    c) 	resource="$resource:ppn=$OPTARG"
         ;;
-    t)  resource=$resource:"walltime=$OPTARG"
+    t)  resource="$resource -l walltime=$OPTARG"
         ;;
     esac
 done
 
-cmd="qsub $resource -d $PWD $SERVICE_DIR/$script"
-echo $cmd
-jobid=`$cmd`
-echo $jobid > jobid
+echo "qsub on $HOSTNAME"
+echo "  $resource"
+echo "  -d $PWD"
+echo "  $SERVICE_DIR/$script"
+
+#needs to be the last command to return exit code
+qsub $resource -d $PWD $SERVICE_DIR/$script > jobid
