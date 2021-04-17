d7ede61f5594a017ea775afbf99e5004da2fd592
gberto <gberto@fbk.eu>
2021-04-17 05:56:28 -0400

    fixed typo when handling func/data and remove zero-cropping if modality is meg or eeg

--- hooks/bl2bids.py
+++ hooks/bl2bids.py
@@ -104,7 +104,9 @@ for id, input in enumerate(config["_inputs"]):
         #raise invalid literal ValueError exception
         try:
             _run = utils.clean(input["meta"]["run"])
-            run = str(int(_run))
+            run = str(int(_run)) #zero-cropped
+            if modality == 'meg' or modality == 'eeg':
+                run = str(_run) #not zero-cropped
             name+="_run-"+run
         except ValueError:
             print("can't parse run.. ignoring", input["meta"]["run"])
@@ -171,19 +173,19 @@ for id, input in enumerate(config["_inputs"]):
         intended_paths.append(dest_under_sub+"_dwi.nii.gz")
 
     elif input["datatype"] == utils.FUNC_TASK:
-        src=os.path.join(input_dir, 'bold.nii.gz'):
+        src=os.path.join(input_dir, 'bold.nii.gz')
         utils.link(src, dest+"_bold.nii.gz")
-        src=os.path.join(input_dir, 'events.tsv'):
+        src=os.path.join(input_dir, 'events.tsv')
         utils.link(src, dest+"_events.tsv")
-        src=os.path.join(input_dir, 'events.json'):
+        src=os.path.join(input_dir, 'events.json')
         utils.link(src, dest+"_events.json")
-        src=os.path.join(input_dir, 'sbref.nii.gz'):
+        src=os.path.join(input_dir, 'sbref.nii.gz')
         utils.link(src, dest+"_sbref.nii.gz")
-        src=os.path.join(input_dir, 'sbref.json'):
+        src=os.path.join(input_dir, 'sbref.json')
         utils.link(src, dest+"_sbref.json")
-        src=os.path.join(input_dir, 'physio.tsv.gz'):
+        src=os.path.join(input_dir, 'physio.tsv.gz')
         utils.link(src, dest+"_physio.tsv.gz")
-        src=os.path.join(input_dir, 'physio.json'):
+        src=os.path.join(input_dir, 'physio.json')
         utils.link(src, dest+"_physio.json")
 
         utils.outputSidecar(dest+"_bold.json", input)
